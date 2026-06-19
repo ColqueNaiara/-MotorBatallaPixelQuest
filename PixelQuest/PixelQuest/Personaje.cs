fe2550b68272ac0b16de936a194d3c61c4c94517
Colque Naiara <naiucwww@gmail.com>
2026-06-18 21:24:41 -0300

    Subo archivos

---
+++ PixelQuest/PixelQuest/Personaje.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelQuest
+{
+    public class Personaje
+    {
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set {
+                if (!string.IsNullOrEmpty(value))
+                    _nombre = value;
+            }
+        }
+        public virtual string Atacar() 
+        {
+            return $"{Nombre} realiza un ataque";
+        }
+    }
+    public class Mago : Personaje {
+        private int _cantidadMana;
+        public int CantidadMana 
+        {
+            get { return _cantidadMana; }
+            set
+            {   if (_cantidadMana<100)
+                _cantidadMana = value;
+            }
+        }
+        public override string Atacar()
+        {
+            return $"{Nombre} lanza una bola de fuego | Maná: {CantidadMana}";
+        }
+    }
+    public class Guerrero : Personaje {
+        private int _resistencia;
+        public int Resistencia
+        {
+            get { return _resistencia; }
+            set
+            {
+                if (_resistencia < 120)
+                _resistencia = value;
+            }
+        }
+        public override string Atacar() 
+        {
+            return $"{Nombre} golpea con su espada | Resistencia: {Resistencia}";
+        }
+    }
+    public class Arquero : Personaje {
+        private int _cantidadFlechas;
+        public int CantidadFlechas
+        {
+            get { return _cantidadFlechas; }
+            set
+            {
+                if (_cantidadFlechas < 130)
+                _cantidadFlechas = value;
+            }
+        }
+        public override string Atacar()
+        {
+            return $"{Nombre} lanza una flecha venenosa | Flechas: {CantidadFlechas}";
+        }
+    }
+}
---
+++ PixelQuest/PixelQuest/Program.cs
@@ -0,0 +1,15 @@
+﻿using PixelQuest;
+
+List<Personaje> personajes = new List<Personaje>();
+
+personajes.Add(new Guerrero {Nombre = "Hercúles", Resistencia = 90 });
+personajes.Add(new Mago { Nombre = "Vandal", CantidadMana = 85 });
+personajes.Add(new Arquero { Nombre = "Sam", CantidadFlechas = 110 });
+
+Console.WriteLine("\t| Ronda de Ataque |");
+
+foreach (var Personaje in personajes) {
+    string mensaje = Personaje.Atacar();
+    Console.WriteLine(mensaje);
+}
+Console.ReadKey();
